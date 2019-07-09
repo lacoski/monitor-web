5cf6b781796012ab9362fb6353158b637a506d52
lacoski <123xuzu@gmail.com>
2019-07-09 08:41:59 +0700

    add http process

--- monitor.sh
+++ monitor.sh
@@ -16,7 +16,6 @@ SwapTotal=$(free -h | grep 'Swap' | awk '{print $2}')
 SwapUsed=$(free -h | grep 'Swap' | awk '{print $3}')
 LoadAvg=$(cat /proc/loadavg | cut -d ' ' -f1,2,3)
 Conn=$(netstat -an |grep 80 | wc -l)
-HttpProc=$(ps -ef |grep httpd | wc -l)
 
 Cpu_Idle=$(top -b -d1 -n1| grep -i "Cpu(s)" | awk '{print $8}')
 if [ $Cpu_Idle == 'id,' ]
@@ -24,6 +23,6 @@ then
     Cpu_Idle='100'
 fi
 
-message="CpuIdle: ${Cpu_Idle} % - Mem: ${MemUsed}/${MemTotal} - Swap: ${SwapUsed}/${SwapTotal} - load: ${LoadAvg} - HttpPro: ${HttpProc} - Conn: ${Conn}"
+message="CpuIdle: ${Cpu_Idle} %  Mem: ${MemUsed}/${MemTotal} - ${SwapUsed}/${SwapTotal} - load: ${LoadAvg} - Conn: ${Conn}"
 #echo $message
 log_script $message
