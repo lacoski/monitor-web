b575d470261bcca8c89c9d96d1ba0cb83471712d
lacoski <123xuzu@gmail.com>
2019-07-09 08:45:36 +0700

    update

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin
-INTERVAL_CHECK='5'
-DIR_SCRIPT=$(pwd)
-while [ true ]; do
-    sleep $INTERVAL_CHECK
-    bash ${DIR_SCRIPT}/monitor.sh
-done
